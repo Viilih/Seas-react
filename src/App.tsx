988d591727a2f8dc768f1dfce343ad6bb78c7dbf
Rafael <rafaelramos0401@gmail.com>
2023-03-17 19:31:38 -0300

    adicionado: componente dos cartões com estilo dinâmico

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import React from "react";
 import "./global.scss";
 import Router from "./route";
 import Transaction from "./components/Transaction/Transaction";
+import Card from "./components/Card/Card";
 
 function App() {
-  return (
-    <>
-      <Router />
-      <Transaction
-        nome="leonardo vasconcelo amaral"
-        valor={-20}
-        data="21/07"
-        dataColor="spent"
-      />
-    </>
-  );
+    return (
+        <>
+            <Router />
+            <Transaction
+                nome="leonardo vasconcelo amaral"
+                valor={-20}
+                data="21/07"
+                dataColor="spent"
+            />
+
+            <Card
+                dataType="platinum"
+                cardNumber={222222222222222}
+                holderName="Claudio Da silva"
+                expiration="02/02"
+            />
+        </>
+    );
 }
 
 export default App;
