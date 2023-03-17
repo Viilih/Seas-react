c92fda8f6965da7586fcea1a62829d1c7f0d9115
Leonardo <leo.vascon.dev@gmail.com>
2023-03-17 18:04:10 -0300

    adicionado componente transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import './global.scss';
-import Router from './route';
+import React from "react";
+import "./global.scss";
+import Router from "./route";
+import Transaction from "./components/Transaction/Transaction";
 
 function App() {
-	return <Router />;
+  return (
+    <>
+      <Router />
+      <Transaction
+        nome="leonardo vasconcelo amaral"
+        valor={-20}
+        data="21/07"
+        dataColor="spent"
+      />
+    </>
+  );
 }
 
 export default App;
--- src/components/Transaction/Transaction.tsx
+++ src/components/Transaction/Transaction.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import styles from './Transaction.module.scss';
+import React from "react";
+import styles from "./Transaction.module.scss";
 
-const Transaction = () => {
-	return <div></div>;
+interface ITransaction {
+  nome: string;
+  valor: number;
+  data: string;
+  dataColor: string;
+}
+
+const Transaction: React.FC<ITransaction> = (ITransaction) => {
+  return (
+    <>
+      <div className={styles.transaction}>
+        <div className={styles.nameData}>
+          <div className={styles.span}></div>
+          <div className={styles.name}>
+            {ITransaction.nome}
+            <div className={styles.data}>{ITransaction.data}</div>
+          </div>
+        </div>
+        <div className={styles.valor} data-color={ITransaction.dataColor}>
+          R$ {ITransaction.valor}
+        </div>
+      </div>
+    </>
+  );
 };
-
 export default Transaction;
