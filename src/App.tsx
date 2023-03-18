c3c66c3c169b4f709c60c1fd359500d426db3961
Leonardo <leo.vascon.dev@gmail.com>
2023-03-18 17:58:57 -0300

    adicionado menu do dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,48 @@ import Router from "./route";
 import Transaction from "./components/Transaction/Transaction";
 import Card from "./components/Card/Card";
 import Inputs from "./components/InputComponents/Inputs";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-    const [userName, setUserName] = useState("");
-    const [userPassword, setuserPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userPassword, setuserPassword] = useState("");
 
-    return (
-        <>
-            <Router />
-            <Transaction
-                nome="leonardo vasconcelo amaral"
-                valor={-20}
-                data="21/07"
-                dataColor="spent"
-            />
+  return (
+    <>
+      <Sidebar />
+      <Router />
+      <Transaction
+        nome="leonardo vasconcelo amaral"
+        valor={20}
+        data="19 fev, 21:30h"
+        dataColor="income"
+      />
 
-            <Card
-                dataType="platinum"
-                cardNumber={222222222222222}
-                holderName="Claudio Da silva"
-                expiration="02/02"
-            />
+      <Card
+        dataType="platinum"
+        cardNumber={222222222222222}
+        holderName="Claudio Da silva"
+        expiration="02/02"
+      />
 
-            <Inputs
-                inputType="text"
-                placeholderText="Informe seu nome"
-                isRequired
-                inputName="name"
-                valueInput={userName}
-                handleChange={(currentValue) => setUserName(currentValue)}
-            />
-            <Inputs
-                inputType="password"
-                placeholderText="Informe sua senha"
-                isRequired
-                inputName="name"
-                valueInput={userPassword}
-                handleChange={(currentValue) => setuserPassword(currentValue)}
-            />
-        </>
-    );
+      <Inputs
+        inputType="text"
+        placeholderText="Informe seu nome"
+        isRequired
+        inputName="name"
+        valueInput={userName}
+        handleChange={(currentValue) => setUserName(currentValue)}
+      />
+      <Inputs
+        inputType="password"
+        placeholderText="Informe sua senha"
+        isRequired
+        inputName="name"
+        valueInput={userPassword}
+        handleChange={(currentValue) => setuserPassword(currentValue)}
+      />
+    </>
+  );
 }
 
 export default App;
