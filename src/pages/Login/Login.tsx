64b2320e7b2323b163ed8f1eb2f8fdf515f7b039
Viilih <gnlnascimento@gmail.com>
2023-03-19 15:43:56 -0300

    Autenticação usuário

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,85 +1,109 @@
-import Inputs from "../../components/InputComponents/Inputs";
-import styles from "./Login.module.scss";
-import { FormEvent, useState } from "react";
+import Inputs from '../../components/InputComponents/Inputs';
+import styles from './Login.module.scss';
+import { FormEvent, useState } from 'react';
 
-import seasLogo from "../../assets/seas-logo.svg";
-import loginImg from "../../assets/login-img.png";
+import seasLogo from '../../assets/seas-logo.svg';
+import loginImg from '../../assets/login-img.png';
 import {
-    ButtonLink,
-    ButtonSubmit,
-} from "../../components/ButtonComponents/Buttons";
-import { Link } from "react-router-dom";
+	ButtonLink,
+	ButtonSubmit,
+} from '../../components/ButtonComponents/Buttons';
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-const Login = () => {
-    const [userCpf, setUserCpf] = useState("");
-    const cpfRegex = `[0-9]{11}`;
+const Login: React.FC = () => {
+	const [userCpf, setUserCpf] = useState('');
+	const cpfRegex = `[0-9]{11}`;
 
-    const [userPassword, setUserPassword] = useState("");
+	const [userPassword, setUserPassword] = useState('');
+	const [toDashboard, setToDashboard] = useState(false);
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        console.log("Enviado", `CPF: ${userCpf}`, `Senha: ${userPassword}`);
-        setUserCpf("");
-        setUserPassword("");
-    };
+	const loginValidation = (cpf: string, password: string) => {
+		const localStorageUser = localStorage.getItem('userInfo');
+		const userInfo = JSON.parse(localStorageUser || '[]');
 
-    return (
-        <div className={styles.loginContainerPage}>
-            <div className={styles.loginContainer}>
-                <div className={styles.loginHeader}>
-                    <img src={seasLogo} alt="Logo do SeasBank" />
-                    <h1>Seja bem-vindo!</h1>
-                </div>
+		const userCredentials = userInfo.map(
+			(user: { CPF: string; Senha: string; Nome: string }) => ({
+				cpf: user.CPF,
+				senha: user.Senha,
+				name: user.Nome,
+			})
+		);
 
-                <form className={styles.formContainer} onSubmit={handleSubmit}>
-                    <Inputs
-                        inputName="cpf"
-                        inputType="text"
-                        isRequired
-                        placeholderText="CPF (somente números)"
-                        valueInput={userCpf}
-                        handleChange={(currentValue) =>
-                            setUserCpf(currentValue)
-                        }
-                        inputPattern={cpfRegex}
-                        inputMinLength={11}
-                        inputMaxLength={11}
-                    />
-                    <Inputs
-                        inputName="inputPassword"
-                        inputType="password"
-                        isRequired
-                        placeholderText="Senha"
-                        valueInput={userPassword}
-                        handleChange={(currentValue) =>
-                            setUserPassword(currentValue)
-                        }
-                        inputMinLength={6}
-                    />
+		const currentUser = userCredentials.find(
+			(user: { cpf: string; senha: string }) =>
+				user.cpf === cpf && user.senha === password
+		);
 
-                    <span>Esqueci minha senha</span>
+		if (currentUser) {
+			setToDashboard(true);
+		} else {
+			alert(
+				'Usuário não encontrado. Verifique os dados inseridos e tente novamente.'
+			);
+		}
+	};
 
-                    <div className={styles.loginBtnContainer}>
-                        <ButtonSubmit
-                            text="Entrar"
-                            name="primary"
-                            btnType="submit"
-                        />
-                        <ButtonLink text="Abrir conta" name="secondary">
-                            <Link to="/register">Abrir conta</Link>
-                        </ButtonLink>
-                    </div>
-                </form>
-            </div>
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log('Enviado', `CPF: ${userCpf}`, `Senha: ${userPassword}`);
+		loginValidation(userCpf, userPassword);
+	};
 
-            <div className={styles.loginImageContainer}>
-                <img
-                    src={loginImg}
-                    alt="imagem de um personagem cartunizado mexendo em um computadorimagem de um personagem cartunizado mexendo em um computador"
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.loginContainerPage}>
+			{toDashboard ? (
+				<Navigate to="/" />
+			) : (
+				<>
+					<div className={styles.loginContainer}>
+						<div className={styles.loginHeader}>
+							<img src={seasLogo} alt="Logo do SeasBank" />
+							<h1>Seja bem-vindo!</h1>
+						</div>
+
+						<form className={styles.formContainer} onSubmit={handleSubmit}>
+							<Inputs
+								inputName="cpf"
+								inputType="text"
+								isRequired
+								placeholderText="CPF (somente números)"
+								valueInput={userCpf}
+								handleChange={currentValue => setUserCpf(currentValue)}
+								inputPattern={cpfRegex}
+								inputMinLength={11}
+								inputMaxLength={11}
+							/>
+							<Inputs
+								inputName="inputPassword"
+								inputType="password"
+								isRequired
+								placeholderText="Senha"
+								valueInput={userPassword}
+								handleChange={currentValue => setUserPassword(currentValue)}
+								inputMinLength={6}
+							/>
+
+							<span>Esqueci minha senha</span>
+
+							<div className={styles.loginBtnContainer}>
+								<ButtonSubmit text="Entrar" name="primary" btnType="submit" />
+								<ButtonLink text="Abrir conta" name="secondary">
+									<Link to="/register">Abrir conta</Link>
+								</ButtonLink>
+							</div>
+						</form>
+					</div>
+					<div className={styles.loginImageContainer}>
+						<img
+							src={loginImg}
+							alt="imagem de um personagem cartunizado mexendo em um computadorimagem de um personagem cartunizado mexendo em um computador"
+						/>
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default Login;
